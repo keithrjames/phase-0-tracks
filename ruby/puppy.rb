33c9edf2eb193932bb71290a55b742d64da868f9
keithrjames <jamekeit@isu.edu>
2016-09-26 10:28:38 -0700

    Finish 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------
+# Puppus Adorableus
+
+# CHARACTERISTICS ---------------
+# Fur Style: Curly
+# Good Boy?: Yes
+# Paws: Adorable
+# Name: Pups
+# Fur Color: Black
+
+# BEHAVIOR ------------------
+# Messy
+# Shake
+# Sit
