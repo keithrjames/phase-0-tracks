0a02f7e4d9a80eb4c122f8e5f9084ea8f585a8c4
keithrjames <jamekeit@isu.edu>
2016-09-27 10:11:43 -0700

    Finish release 1

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,34 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    puts "Woof!" * int
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+    dog_years = human_years * 7
+    puts dog_years
+  end
+
+  def broken_dog
+    puts "Meow!"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
 end
 
+#####DRIVER CODE######
+
+boxer = Puppy.new
+boxer.fetch("bike")
+boxer.speak(5)
+boxer.roll_over
+boxer.dog_years(4)
+boxer.broken_dog
