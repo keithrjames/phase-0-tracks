422bbe6affc897f8612ac2404c226355434c7b85
keithrjames <jamekeit@isu.edu>
2016-09-28 10:51:30 -0700

    Finish release 2. Added getter and setter methods along with reindeer ranking and birthday functions.

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,6 @@
 class Santa
+
+  # Instance Methods
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
   end
@@ -19,25 +21,55 @@ class Santa
       puts "Doris, you are fooling no one. Get out of that stroller."
     end
   end
+  def celebrate_birthday
+    @age += 1
+  end
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.insert(-1,@reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer_name)))
+    puts @reindeer_ranking
+  end
+
+  # Getter Methods
+  def gender
+    @gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  #Setter Methods
+  def gender=(get_gender)
+    @gender = get_gender
+  end
+
 end
 
 
 #####Driver code#####
 
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_names = ["Ken", "Keith", "Samantha", "Doris", "Bobby", "Ashley", "Goober" ]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i], example_names[i])
-end
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_names = ["Ken", "Keith", "Samantha", "Doris", "Bobby", "Ashley", "Goober" ]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i], example_names[i])
+# end
 
-puts santas
+# puts santas
 
-santas.each do |santa|
-  puts santa.doris_is_old
-end
+# santas.each do |santa|
+#   puts santa.doris_is_old
+# end
 
-# cris = Santa.new
-# cris.speak
-# cris.eat_milk_and_cookies("fudge")
+cris = Santa.new("Man", "White", "Keith")
+cris.speak
+cris.eat_milk_and_cookies("fudge")
+puts cris.celebrate_birthday
+puts cris.get_mad_at("Comet")
+cris.gender = "Bone Doggy"
+puts "my name gender is #{cris.gender}"
