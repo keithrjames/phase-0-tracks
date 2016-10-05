f8d8b5cf545b9b47d4e0c0fd7f5319ce41ad51a1
keithrjames <jamekeit@isu.edu>
2016-10-05 10:30:21 -0700

    Finish release 2. Created two new instances of the Car object.

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,4 +14,30 @@ for (var i = 0; i < colors.length; i++){
 
 console.log(horses)
 
+function Car(model, speed, color) {
+  console.log("Our Car:", this);
+
+    this.model = model;
+    this.speed = speed;
+    this.color = color;
+
+    this.TurnCarOn = function(){
+      console.log("Vroom Vroom!");
+    };
+
+    console.log("CAR INITIALIZATION COMPLETE");
+}
+
+var newcar = new Car("Ford", "60mph", "Black");
+console.log(newcar);
+newcar.TurnCarOn();
+
+var newcartwo = new Car("Prius", "55mph", "Red");
+console.log(newcartwo);
+newcartwo.TurnCarOn();
+
+var newcarthree = new Car("Jeep", "80mph", "White");
+console.log(newcarthree);
+newcarthree.TurnCarOn();
+
 
