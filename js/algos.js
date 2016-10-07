e45a0cd227fd9239257cd00c196786b20f33a61a
keithrjames <jamekeit@isu.edu>
2016-10-07 12:09:13 -0700

    Finish release 1. Function can now determine if there is at least one key value pair that matches.

--- js/algos.js
+++ js/algos.js
@@ -19,6 +19,26 @@ function longestPhrase(array) {
   console.log(largest);
 }
 
+function keyValueMatch(object1, object2) {
+  var match = false
+  for (i in object1){
+    for (x in object2){
+      if (i == x && object1[i] == object2[x]) {
+        var match = true
+        break;
+      }
+    }
+  }
+    return match
+}
+
+
+///Driver Code///
+
 longestPhrase(["apple", "zebras", "helecopter"]);
 longestPhrase(["keith", "bucket", "dinosaur"]);
 longestPhrase(["chimney", "cat", "rabbit"]);
+
+console.log(keyValueMatch({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+console.log(keyValueMatch({age: 54, name: "Steven"}, {name: "Tamir", age: 54}));
+console.log(keyValueMatch({salad: "gurt", golf: "Yes"}, {salame: 45, buckets: "All day"}));
