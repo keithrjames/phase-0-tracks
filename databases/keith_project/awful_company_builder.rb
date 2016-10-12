be2f8d6b394c22af453b2186f8684f5e42601b16
keithrjames <jamekeit@isu.edu>
2016-10-12 15:12:25 -0700

    Create ability for user to get a new company.

--- databases/keith_project/awful_company_builder.rb
+++ databases/keith_project/awful_company_builder.rb
@@ -19,9 +19,9 @@ def create_business(db, company_name, slogan, address, logo)
   db.execute("INSERT INTO business (company_name, slogan, address, logo) VALUES (?, ?, ?, ?);", [company_name, slogan, address, logo])
 end
 
-# 1000.times do
-#   create_business(db, Faker::Company.name, Faker::Company.bs, Faker::Address.street_address, Faker::Company.logo)
-# end
+1000.times do
+   create_business(db, Faker::Company.name, Faker::Company.bs, Faker::Address.street_address, Faker::Company.logo)
+end
 
 # explore ORM by retrieving data
 
@@ -29,7 +29,7 @@ business = db.execute("SELECT * FROM business ORDER BY RANDOM() LIMIT 1;")
 
 # DRIVER CODE
 
-puts <<-STRING
+puts generator =  <<-STRING
 Welcome to the random business generator!!!
 
 Now creating your random business...
@@ -43,4 +43,25 @@ Slogan: #{business[0][2]}
 Address: #{business[0][3]}
 View your logo here: #{business[0][4]}
 STRING
+
+answer = ""
+
+while answer != "y"
+  puts "Do you like your business?(y/n)"
+  answer = gets.chomp.downcase
+  business = db.execute("SELECT * FROM business ORDER BY RANDOM() LIMIT 1;")
+  generator = <<-STRING
+Now creating your random business...
+
+...
+...
+...
+
+Name: #{business[0][1]}
+Slogan: #{business[0][2]}
+Address: #{business[0][3]}
+View your logo here: #{business[0][4]}
+STRING
+  puts generator
+end
 
